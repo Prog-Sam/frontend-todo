53c6f1e99820c606d1084abea992b0c967e7d40f
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2024-04-18 22:02:24 +0800

    Created Incomplete Todo.jsx

---
+++ src/components/Todo.jsx
@@ -0,0 +1,43 @@
+import '../css/Todo.css';
+
+const Todo = ({ todo, onUpdate, onDelete, onChange, editable }) => {
+    return (<div key={todo.id} className="d-flex justify-content-start border border-secondary">
+        <div className="p-2 flex-fill align-self-center">
+            {
+                editable ?
+                    (
+                        <input
+                            className="form-control"
+                            type='text'
+                            value={todo.task}
+                            onChange={onChange}
+                        />
+                    ) :
+                    (
+                        <h4
+                            onClick={() => onUpdate({ ...todo, ['completed']: !todo.completed })}
+                            className={todo.completed ? "Todo-task completed" : "Todo-task"}
+                        >{todo.task}</h4>
+                    )
+            }
+        </div>
+        {!editable &&
+            <>
+                <div className="p-2">
+                    <button className='btn btn-warning'>Edit</button>
+                </div>
+                <div className="p-2">
+                    <button onClick={() => onDelete(todo.id)} className='btn btn-danger'>Delete</button>
+                </div>
+            </>
+        }
+        {
+            editable &&
+            <div className="p-2">
+                <button className='btn btn-success'>Save</button>
+            </div>
+        }
+    </div>);
+}
+
+export default Todo;
