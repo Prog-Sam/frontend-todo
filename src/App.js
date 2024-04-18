a67ac0968c95a34984ab4e96363f963a55fad7ce
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2024-04-19 00:56:36 +0800

    Updated Class for better design in app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
   return (
-    <div className='back-ground-level-0' >
+    <div className='back-ground-level-0 box' style={{ height: '100%', width: '100%' }}>
       <div className="App center border border-secondary ">
         <TodoList />
       </div>
