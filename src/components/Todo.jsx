439408e10f2a002c3182d06dab79ff1e318f71fb
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2024-04-19 02:13:39 +0800

    Integrated ToDo and update ToDo Form

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,42 +1,32 @@
+import { useEffect, useState } from 'react';
 import '../css/Todo.css';
+import UpdateTodoForm from './UpdateTodoForm';
 
-const Todo = ({ todo, onUpdate, onDelete, onChange, editable }) => {
+const Todo = ({ todo, onUpdate, onDelete }) => {
+    const [editable, setEditable] = useState(false);
+
     return (<div key={todo.id} className="d-flex justify-content-start border border-secondary">
-        <div className="p-2 flex-fill align-self-center">
-            {
-                editable ?
-                    (
-                        <input
-                            className="form-control"
-                            type='text'
-                            value={todo.task}
-                            onChange={onChange}
-                        />
-                    ) :
-                    (
+        {!editable &&
+            <>
+                <div className="p-2 flex-fill align-self-center">
+                    {
                         <h4
                             onClick={() => onUpdate({ ...todo, ['completed']: !todo.completed })}
                             className={todo.completed ? "Todo-task completed" : "Todo-task"}
                             style={{ cursor: 'pointer' }}
                         >{todo.task}</h4>
-                    )
-            }
-        </div>
-        {!editable &&
-            <>
+                    }
+                </div>
                 <div className="p-2">
-                    <button className='btn btn-warning'>Edit</button>
+                    <button className='btn btn-warning' onClick={() => setEditable(true)}>Edit</button>
                 </div>
                 <div className="p-2">
                     <button onClick={() => onDelete(todo.id)} className='btn btn-danger'>Delete</button>
                 </div>
             </>
         }
-        {
-            editable &&
-            <div className="p-2">
-                <button className='btn btn-success'>Save</button>
-            </div>
+        {editable &&
+            <UpdateTodoForm data={todo} setEditable={setEditable} onUpdate={onUpdate} />
         }
     </div>);
 }
