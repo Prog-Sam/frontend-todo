f1536660a9f3850c4fc6bd852b4cba43cc2d53f1
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2024-04-18 23:24:56 +0800

    created httpService

---
+++ src/services/httpService.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+// Our Backend's address
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+
+// For error handling
+axios.interceptors.response.use(null, (error) => {
+    const expectedError =
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status < 500;
+
+    if (!expectedError) {
+        console.error('An unexpected error occured.');
+        console.error('Logging the error', error);
+    }
+    return Promise.reject(error);
+});
+
+
+export default {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete,
+};
