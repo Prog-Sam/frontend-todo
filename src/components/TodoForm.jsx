0ce696f1d527bf6d1b27028c5ec428aec1d51846
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2024-04-19 00:54:56 +0800

    Todo Form Added

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { saveTodo } from "../services/todoService";
+
+
+//Schema for validation added here
+
+const TodoForm = ({ refreshTodos }) => {
+    //Initialize State
+    const [todo, setTodo] = useState({ task: '', completed: false });
+
+    const handleSubmit = async (e) => {
+        //To Avoid page reload
+        e.preventDefault();
+
+        //Calling the database
+        await saveTodo(todo);
+        //Updating the the todo list
+        refreshTodos();
+        //Reinitializing state
+        setTodo({ task: '', completed: false });
+    }
+
+    return (
+        <form className='col-8 back-ground-level-1' onSubmit={handleSubmit} style={{ marginTop: '50px' }}>
+            <h3 className='d-flex align-items-left' htmlFor="task">New todo</h3>
+            <div className='d-flex'>
+                <div className='p-2'>
+                    <input
+                        value={todo.task}
+                        onChange={(e) => setTodo({ ...todo, [e.target.name]: e.target.value })}
+                        id="task"
+                        type="text"
+                        name="task"
+                        placeholder="New Todo"
+                        className='d-flex form-control'
+                    />
+                </div>
+                <div className='p-2'>
+                    <button disabled={todo.task === ''} className='btn btn-primary'>Add Todo</button>
+                </div>
+            </div>
+        </form>
+    );
+}
+
+export default TodoForm;
