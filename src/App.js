8cbc506f25f5fcc46e110a01443fdda909d0db2e
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2024-04-18 23:18:05 +0800

    Cleaned App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import TodoList from './components/TodoList';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <div className=''>
-        <h1>Todo List</h1>
-        <h5>A Simple Todo List</h5>
-        <hr />
+    <div className='back-ground-level-0' >
+      <div className="App center border border-secondary ">
+        <TodoList />
       </div>
     </div>
   );
